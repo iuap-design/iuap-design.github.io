f7b407209cab7f15221a781efa6c947f73456dea
wanghaoo <wanghao@yonyou.com>
2016-07-05 20:50:57 +0800

    增加顶部layout布局示例

---
+++ dist/pages/webIDE/demos/layouts/topNavLayout/widget.js
@@ -0,0 +1,31 @@
+u.on(window, 'load', function() {
+    'use strict';
+
+
+    // 点击屏幕dom元素时去掉menuNav中的open样式
+    $(document).on('click', function(e) {
+        if ($(event.target).hasClass('.site-menu-item') || $(event.target).parents('.site-menu-item').length > 0) {
+                return;
+            } else {
+                $('.site-menu-item').removeClass('open');
+            }
+
+        })
+        // menuNav点击时，显示子内容
+    $('.site-menu-item.has-sub a').click(function() {
+        $(this).parent().toggleClass('open');
+    })
+
+    // 汉堡点击效果，menuNav收起和隐藏
+    $('.hamburger').click(function() {
+        $(this).toggleClass('hided');
+        if ($('body').hasClass('site-menubar-hide')) {
+            $('body').removeClass('site-menubar-hide');
+            $('body').addClass('site-menubar-open');
+        } else {
+            $('body').removeClass('site-menubar-open');
+            $('body').addClass('site-menubar-hide');
+        }
+    });
+
+})
