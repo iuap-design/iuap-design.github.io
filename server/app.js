e78a139b332279ff360f289310f1390806f7eea1
guoyff <guoyff@yonyou.com>
2016-07-06 11:11:06 +0800

    改变首页服务地址

--- server/app.js
+++ server/app.js
@@ -8,13 +8,13 @@ var gzip = require('koa-gzip');
 
 
 var app = koa();
-var iwebRouter = require('./router');
-
-// uui定制
-var customized = require("./customized");
-var down = require("./down");
-
-iwebRouter.setRouters(router, '.');
+// var iwebRouter = require('./router');
+//
+// // uui定制
+// var customized = require("./customized");
+// var down = require("./down");
+//
+// iwebRouter.setRouters(router, '.');
 
 app.use(gzip());
 
@@ -26,6 +26,16 @@ app.use(koaBody({
 
 app.use(router.routes())
   .use(router.allowedMethods());
+
+// response
+app.use(function *(next){
+  // (3) 进入 response 中间件，没有捕获到下一个符合条件的中间件，传递到 upstream
+  if (this.request.url === '/') {
+    this.body = fs.readFileSync(path.resolve(__dirname, '../dist/index.html')).toString();
+  } else {
+    return yield* next;
+  }
+});
 
 app.use(serve(path.join(__dirname, '../')));
 
