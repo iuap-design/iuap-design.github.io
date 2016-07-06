e5984e614899ee6f495d8690361b3f2b410f8f99
liuyk <liuyk@yonyou.com>
2016-07-06 16:55:38 +0800

    修改webide下悬浮页面框架布局

--- dist/pages/webIDE/demos/layouts/iconFrame/widget.js
+++ dist/pages/webIDE/demos/layouts/iconFrame/widget.js
@@ -1,1 +1,10 @@
-debugger;//Site.run();
+
+
+document.write('<' + 'script src="dist/static/js/iconFramePlugins/jquery-asHoverScroll.js"><' + '/script>')
+document.write('<' + 'script src="dist/static/js/iconFramePlugins/core.js"><' + '/script>')
+document.write('<' + 'script src="dist/static/js/iconFramePlugins/site.js"><' + '/script>')
+document.write('<' + 'script src="dist/static/js/iconFramePlugins/menu.js"><' + '/script>')
+document.write('<' + 'script src="dist/static/js/iconFramePlugins/menubar.js"><' + '/script>')
+setTimeout(function(){
+	Site.run();	
+},1000)
