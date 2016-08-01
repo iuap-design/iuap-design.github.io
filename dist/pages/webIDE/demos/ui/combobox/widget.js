6cb65b07161015759c6d56adfbc9fdd74792df4b
onvno <leewei2020@126.com>
2016-08-01 11:00:02 +0800

    fix:webide左侧导航内容合并

--- dist/pages/webIDE/demos/ui/combobox/widget.js
+++ dist/pages/webIDE/demos/ui/combobox/widget.js
@@ -31,3 +31,53 @@ document.getElementById('combo2')['u.Combo'].setComboData([{value:'01',name:'男
 	    }
 	}
 // 输入框结束
+
+// 自动完成开始
+	var sourceValue = [
+		{
+			label : "C++",
+			value: "C++"
+		},
+		{
+			label : 'Java',
+			value: 'Java'
+		},
+		{
+			label : 'Python',
+			value: 'Python'
+		},
+		{
+			label : 'JavaScript',
+			value: 'JavaScript'
+		},
+		{
+			label : 'C#',
+			value: 'C#'
+		},
+		{
+			label : 'C',
+			value: 'C'
+		},
+		{
+			label : 'Jython',
+			value: 'Jython'
+		},
+		{
+			label : 'Html',
+			value: 'Html'
+		},
+		{
+			label : 'html',
+			value: 'html'
+		}
+	];
+
+	new u.Autocomplete({
+		el:'#test1',
+		source: sourceValue,
+		multiSelect: true,
+		select: function(item, ac){
+		}
+	});
+// 自动完成结束
+  
--- dist/pages/webIDE/demos/ui/dialog/widget.js
+++ dist/pages/webIDE/demos/ui/dialog/widget.js
@@ -1,43 +1,100 @@
-u.compMgr.apply({
-    el: 'body'
-})
+// 消息框弹出开始
+    u.compMgr.apply({
+        el: 'body'
+    })
 
-var msgBtn = document.body.querySelector("#msgDialogBtn");
-u.on(msgBtn, 'click', function() {
-    //            u.showMessage("HELLO!!!");
-    u.messageDialog({ msg: "HELLO!!!", title: "测试提示", btnText: "OK!" });
-})
+    var msgBtn = document.body.querySelector("#msgDialogBtn");
+    u.on(msgBtn, 'click', function() {
+        //            u.showMessage("HELLO!!!");
+        u.messageDialog({ msg: "HELLO!!!", title: "测试提示", btnText: "OK!" });
+    })
 
-//  确认消息框
-var msgBtn2 = document.body.querySelector("#msgDialogBtn2");
-u.on(msgBtn2, 'click', function() {
-    //            u.showMessage("HELLO!!!");
-    u.confirmDialog({
-        msg: "是否保存单据？",
-        title: "测试确认",
-        onOk: function() {
-            alert('ok')
-        },
-        onCancel: function() {
-            alert('cancel')
+    //  确认消息框
+    var msgBtn2 = document.body.querySelector("#msgDialogBtn2");
+    u.on(msgBtn2, 'click', function() {
+        //            u.showMessage("HELLO!!!");
+        u.confirmDialog({
+            msg: "是否保存单据？",
+            title: "测试确认",
+            onOk: function() {
+                alert('ok')
+            },
+            onCancel: function() {
+                alert('cancel')
+            }
+        });
+    })
+
+
+
+    var okButton = document.body.querySelector(".u-msg-ok");
+    u.on(okButton, 'click', function() {
+        alert('ok');
+    });
+
+    var cancelButton = document.body.querySelector(".u-msg-cancel");
+    u.on(cancelButton, 'click', function() {
+        md.close();
+    });
+
+    // 模态框
+    var msgBtn3 = document.body.querySelector("#msgDialogBtn3");
+    u.on(msgBtn3, 'click', function() {
+        window.md = u.dialog({ id: 'testDialg', content: "#dialog_content", hasCloseMenu: true });
+    });
+// 消息框弹出结束
+
+// 消息提示开始
+    var msgBtn = document.body.querySelector("#msgBtn");
+    var errorBtn = document.body.querySelector("#errorBtn");
+    var warnBtn = document.body.querySelector("#warnBtn");
+    var rightInfo='<i class="uf uf-checkedsymbol margin-r-5"></i>成功信息!!!';
+    u.on(msgBtn,'click', function(){ 
+        u.showMessage({msg:rightInfo,position:"center"})
+    })
+
+    var errorInfo='<i class="uf uf-crossmarkonablackcirclebackground margin-r-5"></i>错误信息!!!'
+    u.on(errorBtn,'click', function(){ 
+        u.showMessage({msg:errorInfo,position:"center",msgType:"error"})
+    })
+
+    var warningInfo='<i class="uf uf-exclamationsign margin-r-5"></i>警告信息!!!';
+    u.on(warnBtn,'click', function(){ 
+        u.showMessage({msg:warningInfo,position:"center",msgType:"warning"})
+    })
+// 消息提示结束
+
+// 工具提示开始
+    $('.u-button-tooltip-left').on('click',function () {
+        var left=$(this).get(0).offsetLeft+$(this).get(0).clientWidth;
+        var top=$(this).get(0).offsetTop;
+        var $tooltip=$(this).next();
+        $tooltip.css('left',left+10);
+        $tooltip.css('top',top);
+        if($tooltip.is(':hidden')){
+            $tooltip.show();
+        }else{
+            $tooltip.hide()
         }
     });
-})
-
-
-
-var okButton = document.body.querySelector(".u-msg-ok");
-u.on(okButton, 'click', function() {
-    alert('ok');
-});
-
-var cancelButton = document.body.querySelector(".u-msg-cancel");
-u.on(cancelButton, 'click', function() {
-    md.close();
-});
-
-// 模态框
-var msgBtn3 = document.body.querySelector("#msgDialogBtn3");
-u.on(msgBtn3, 'click', function() {
-    window.md = u.dialog({ id: 'testDialg', content: "#dialog_content", hasCloseMenu: true });
-});
+    $('.tooltip-rotate').each(function () {
+        var $btn=$(this).prev();
+        $(this).css('left',$btn.get(0).clientWidth+$btn.get(0).offsetLeft+10);
+        $(this).css('top',$btn.get(0).offsetTop);
+    });
+    $('.u-button-tooltip-rotate').hover(function () {
+        var left=$(this).get(0).clientWidth;
+        var top=$(this).get(0).offsetTop;
+        var $tooltip=$(this).next();
+        $tooltip.css('left',left+40);
+        $tooltip.css('top',top);
+        $tooltip.addClass('rotate');
+    },function () {
+        var left=$(this).get(0).clientWidth;
+        var top=$(this).get(0).offsetTop;
+        var $tooltip=$(this).next();
+        $tooltip.css('left',left+40);
+        $tooltip.css('top',top);
+        $tooltip.removeClass('rotate');
+    });
+// 工具提示结束
