8763ddb8502bddd1aa178b7d68bc05e2c7ef0097
胡玥 <hy1062887235@163.com>
2016-08-26 15:57:53 +0800

    增加基本控件文字提示

--- dist/pages/webIDE/demos/complex/transfer-dt/widget.js
+++ dist/pages/webIDE/demos/complex/transfer-dt/widget.js
@@ -4,14 +4,14 @@ var app, viewModel, basicDatas, computes, events;
 
 basicDatas = {
     srcListData: new u.DataTable({
-        meta: { 
+        meta: {
             field1: {
                 type: 'string'
             }
         }
     }),
     destListData: new u.DataTable({
-        meta: { 
+        meta: {
             field1: {
                 type: 'string'
             }
@@ -21,7 +21,7 @@ basicDatas = {
 };
 
 computes = {
-    
+
 }
 
 events = {
@@ -51,10 +51,13 @@ events = {
         viewModel.destListData.clear();
     },
     leftSelAll: function() {
-        
-        viewModel.srcListData.toggleAllSelect()
-    }
 
+        viewModel.srcListData.toggleAllSelect()
+    },
+    rightSelAll: function() {
+
+        viewModel.destListData.toggleAllSelect()
+    }
 }
 
 
@@ -114,10 +117,9 @@ var init = function() {
         el: '.demo',
         model: viewModel
     })
-    
+
     getInitData()
-    
+
 }
 
 init()
-
