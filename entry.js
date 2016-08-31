852bdfdf2219de1242050c00f50fc225e9bfa18f
onvno <leewei2020@126.com>
2016-08-31 21:23:22 +0800

    chore: 搭建框架输出机制

--- entry.js
+++ entry.js
@@ -1,18 +1,23 @@
 //Sparrow import
 import {extend} from 'neoui-sparrow/lib/extend';
 
-//Neoui import
+// import {Autocomplete} from 'neoui/js/neoui-autocomplete';
+// import {Button} from 'neoui/js/neoui-button';
+
+
+
+// var ex = {
+// 	Autocomplete : Autocomplete,
+// 	Button : Button
+// };
+
+// extend(ex,window.u || {});
+// window.u = ex;
+// export {ex as u};
+
 import {Autocomplete} from 'neoui/js/neoui-autocomplete';
 import {Button} from 'neoui/js/neoui-button';
-
-
-
-var ex = {
-	Autocomplete : Autocomplete,
-	Button : Button
-};
-//extend(ex,env);
-
+var ex = {"Autocomplete":Autocomplete,"Button":Button}
 extend(ex,window.u || {});
 window.u = ex;
 export {ex as u};
--- server/pack.js
+++ server/pack.js
@@ -6,8 +6,11 @@ var concat = require('gulp-concat');
 var webpack = require('gulp-webpack');
 var rename = require('gulp-rename');
 
-// 获取Neoui的scss & js目录 
-// var neouijson = require('./neoui.json');
+var fse = require('fs-extra');
+
+// 获取Neoui es6模块依赖关系
+var neojson = require('./neoui.json');
+var neoModule = neojson.es6;
 
 
 module.exports = function(data){
@@ -67,6 +70,42 @@ module.exports = function(data){
 		  neouiJs.push(neouiBasePath + '/js/' + dataJson.neoselect[i] + '.js');
 		}
 	}
+
+	// 入口文件内容
+	var entryPath = path.resolve(__dirname,'../entry.js');
+	var dataNeo = [];
+	var ex = {};
+
+	var entryFun = function() {
+		for(var i=0, neoLength = dataJson.neoselect.length; i < neoLength; i++ ) {
+			var pluginModule = neoModule[dataJson.neoselect[i]];
+			for (var key in pluginModule) {
+				dataNeo.push(pluginModule[key]);
+				ex[key] = key;
+			}
+		}
+	}
+	entryFun();
+
+	// 写入入口文件
+	var dataNeoStr = dataNeo.join('\n');
+	fs.appendFileSync(entryPath,dataNeoStr);
+	var exBefore = '\nvar ex = ';
+	var exStr = JSON.stringify(ex);
+	var exAfter = [
+		"\nextend(ex,window.u || {});",
+		"window.u = ex;",
+		"export {ex as u};"
+	].join("\n");
+	var exportStr = exBefore + exStr + exAfter;
+	fs.appendFileSync(entryPath,exportStr);
+
+	// 入口文件字符串替换
+	var dataFsOrigin = fs.readFileSync(entryPath, 'utf-8');
+	var dataFs = dataFsOrigin.replace(/:"([\w-]+)"/g,":$1");
+	fs.writeFileSync(entryPath, dataFs);
+	
+	
 
 	// sass部分
 	gulp.task('sass', function() {
