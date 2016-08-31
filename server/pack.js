52d89c4b746c51f9ecaaab725d0b74bdc60f975b
onvno <leewei2020@126.com>
2016-08-31 11:02:07 +0800

    chore: 实现polyfill输出

--- server/pack.js
+++ server/pack.js
@@ -4,27 +4,83 @@ var sass = require('gulp-sass');
 var path = require('path');
 var concat = require('gulp-concat');
 var webpack = require('gulp-webpack');
+var rename = require('gulp-rename');
 
 
 module.exports = function(data){
 
+	/**
+	 * Poly Match: polyselect
+	 * Neoui Match: neoselect
+	 */
 	var dataJson = data;
 	var basePath = '../node_modules/';
-	var neouiBasePath = path.resolve(__dirname,basePath + 'neoui');
-	var neouiCss =[];
-	var neouiJs =[];
-	if(dataJson.neoselect) {
-		for(var i=0; i<dataJson.neoselect.length; i++) {
-		  neouiCss.push(neouiBasePath + '/scss/ui/' + dataJson.neoselect[i] + '.scss');
-		  neouiJs.push(neouiBasePath + '/js/' + dataJson.neoselect[i] + '.js');
-		}
-	}
 
-	gulp.task('concat', function() {
-		return gulp.src(neouiCss)
-			.pipe(concat('te'))
-			.pipe(gulp.dest(path.resolve(__dirname,'../')))
-	});
+	/**
+	 * polyfill定制部分
+	 */
+	var polyBasePath = path.resolve(__dirname, basePath + 'neoui-polyfill');
+	var polyJs = [];
+
+	var polyFun = function(){
+		if(dataJson.polyselect) {
+			for(var i = 0, polyLen = dataJson.polyselect.length; i < polyLen; i++) {
+				polyJs.push(polyBasePath + '/dist/' + dataJson.polyselect[i] + '.js');
+			}
+		}
+		
+		gulp.task('poly', function(){
+
+			if(polyJs.length === 2){
+				return gulp.src(polyJs)
+					.pipe(concat('u-polyfill.js'))
+					.pipe(gulp.dest(path.resolve(__dirname,'../')));
+			} else if (polyJs.length === 1 && polyJs[0] === 'u-polyfill-core') {
+				return gulp.src(polyJs)
+					.pipe(rename('u-polyfill.js'))
+					.pipe(gulp.dest(path.resolve(__dirname,'../')));
+			} else if (polyJs.length === 1 && polyJs[0] === 'u-polyfill-repsond') {
+				return gulp.src(polyJs)
+					.pipe(rename('u-polyfill.js'))
+					.pipe(gulp.dest(path.resolve(__dirname,'../')));
+			}
+		});
+
+		gulp.run('poly');
+	};
+	polyFun();
+	
+
+	
+
+
+
+	/**
+	 * neoui定制部分
+	 */
+	// var neouiBasePath = path.resolve(__dirname,basePath + 'neoui');
+	// var neouiCss =[];
+	// var neouiJs =[];
+	// if(dataJson.neoselect) {
+	// 	for(var i=0; i<dataJson.neoselect.length; i++) {
+	// 	  neouiCss.push(neouiBasePath + '/scss/ui/' + dataJson.neoselect[i] + '.scss');
+	// 	  neouiJs.push(neouiBasePath + '/js/' + dataJson.neoselect[i] + '.js');
+	// 	}
+	// }
+
+	// gulp.task('concat', function() {
+	// 	return gulp.src(neouiCss)
+	// 		.pipe(concat('te'))
+	// 		.pipe(gulp.dest(path.resolve(__dirname,'../')))
+	// });
+
+
+
+
+
+	/**
+	 * webpack部分
+	 */
 
 	// gulp.task('webpack', function() {
 	// 	return gulp.src('')
@@ -37,18 +93,11 @@ module.exports = function(data){
 	// 			},
 	// 			resolve:{
 					
-	// 			}
-	// 		))
+	// 			}	// 		))
 	// 		.pipe(gulp.dest(''));
 	// })
 
 
 
-
-
-
-
-
-
-	gulp.run('concat');
+	// gulp.run('concat');
 };
