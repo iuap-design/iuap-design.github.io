138212e7ecf50fef55528cd59e176b4dc77f18d9
Boyuzhou <2470200492@qq.com>
2016-09-01 16:34:42 +0800

    add neoui select menu

--- dist/static/js/scripts.js
+++ dist/static/js/scripts.js
@@ -962,4 +962,27 @@ var clickHandler = function (e) {
 
 $(document)
   .on('click.bs.tab.data-api', '[data-toggle="tab"]', clickHandler)
-  .on('click.bs.tab.data-api', '[data-toggle="pill"]', clickHandler)
+  .on('click.bs.tab.data-api', '[data-toggle="pill"]', clickHandler);
+
+  $('.u-button-hover').hover(function () {
+      var offset = $(this).offset();
+	  $(this).next().css('left', offset.left);
+	   $(this).next().show();
+  },function () {
+	  var flag=true;
+	  var time=$(this).attr('time')||0;
+	  var $that=$(this).next();
+	  $that.hover(function () {
+		  flag=false;
+		  $that.show();
+	  },function () {
+		  window.setTimeout(function () {
+			  $that.hide();
+		  },time);
+	  });
+	  window.setTimeout(function () {
+		  if(flag){
+			  $that.hide();
+		  }
+	  },time?time:100);
+  });
