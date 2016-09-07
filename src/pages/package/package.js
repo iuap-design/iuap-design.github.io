43311b24becba87187ec9566fe5c36c3e7914766
onvno <leewei2020@126.com>
2016-09-07 16:05:22 +0800

    chore: 增加颜色选择

--- src/pages/package/package.js
+++ src/pages/package/package.js
@@ -1,4 +1,101 @@
 window.onload = function() {
+    /**
+     * 颜色选中弹出picker
+     */
+    var inColor = document.getElementById('color0');
+    var colorGroup = document.getElementById('colorGroup');
+    var colorShow = document.getElementById('colorShow');
+    var picker = {
+        init: function(){
+            /**
+             * 触发显示
+             */
+            var self = this
+            self.focus(inColor);
+
+            /**
+             * hover事件
+             */
+            var themeCors = colorGroup.getElementsByTagName('li');
+            var objEach = Array.prototype.forEach;
+            objEach.call(themeCors,function(ele){
+                self.corhover(ele);
+                self.corselect(ele);
+            });
+
+            // document.addEventListener('click',function(){
+            //     colorGroup.style.display = 'none';
+            // },false);
+        },
+        init_ui: function(){
+            var progress = document.querySelectorAll('#tab-panel-1 .progressbar');
+            for(var i=0; i<progress.length; i++){
+                progress[i].style.width = '50%';
+            }
+        },
+        focus: function(inele){
+            var self = this;
+            inele.addEventListener('focus', function(event){
+                /**
+                 * show colorGroup
+                 */
+                var et = event.target || event.srcElement;
+                var getX = et.offsetLeft;
+                var getY = et.offsetTop;
+                var getWidth = parseInt(getComputedStyle(et,null).getPropertyValue('width'));
+                var setLeft = getX + getWidth + 'px';
+                var setTop = getY + 'px';
+
+                var setStyle = {
+                    display: "block",
+                    left: setLeft,
+                    top: setTop
+                };
+                for(var key in setStyle) {
+                    colorGroup.style[key] = setStyle[key];
+                }
+                event.stopPropagation();
+            });
+        },
+        corhover: function(ele){
+            ele.addEventListener('mouseenter', function(event){
+                var eleCor = ele.getAttribute('data-color');
+                colorShow.style.backgroundColor = eleCor;
+            },true);
+        },
+        corselect: function(ele){
+            var self = this;
+            ele.addEventListener('click', function(event){
+                var eleCor = ele.getAttribute('data-color');
+                colorGroup.style.display = 'none';
+                inColor.value = eleCor;
+                self.uichange(eleCor);
+
+                event.stopPropagation();
+            },true);
+
+        },
+        uichange: function(selColor){
+            var uiObj = {
+                "nav": "#tab-panel-1 .u-navbar-inverse",
+                "button": "#tab-panel-1 .u-button",
+                "progress": "#tab-panel-1 .progressbar",
+                "switch": "#tab-panel-1 .u-switch-track",
+                "pagination":"#tab-panel-1 #pagination .active a"
+            };
+
+            for(var key in uiObj) {
+                var catUis = document.querySelectorAll(uiObj[key]);
+                for(var i = 0; i<catUis.length; i++){
+                    catUis[i].style.backgroundColor = selColor;
+                }
+            }
+
+        }
+    };
+    picker.init_ui();
+    picker.init();
+
 
     /**
      * 选中元素,执行ajax
