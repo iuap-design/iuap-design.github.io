ec81198405b36d7b078f5b9f4f3ced3c03b1885c
Boyuzhou <2470200492@qq.com>
2016-09-07 09:21:15 +0800

    dropdown js fix

--- dist/static/js/dropdown-hover.js
+++ dist/static/js/dropdown-hover.js
@@ -1,22 +1,24 @@
-$('.u-button-hover').hover(function () {
-    var offset = $(this).offset();
-    $(this).next().css('left', offset.left);
-     $(this).next().show();
-},function () {
-    var flag=true;
-    var time=$(this).attr('time')||0;
-    var $that=$(this).next();
-    $that.hover(function () {
-        flag=false;
-        $that.show();
+$(document).ready(function () {
+    $('.u-button-hover').hover(function () {
+        var offset = $(this).offset();
+        $(this).next().css('left', offset.left);
+         $(this).next().show();
     },function () {
+        var flag=true;
+        var time=$(this).attr('time')||0;
+        var $that=$(this).next();
+        $that.hover(function () {
+            flag=false;
+            $that.show();
+        },function () {
+            window.setTimeout(function () {
+                $that.hide();
+            },time);
+        });
         window.setTimeout(function () {
-            $that.hide();
-        },time);
+            if(flag){
+                $that.hide();
+            }
+        },time?time:100);
     });
-    window.setTimeout(function () {
-        if(flag){
-            $that.hide();
-        }
-    },time?time:100);
 });
