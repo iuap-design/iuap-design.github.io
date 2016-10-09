25b2d709731f9562b484d2474bb3469f00fb6c8f
胡玥 <hy1062887235@163.com>
2016-10-09 11:03:53 +0800

    修改树的反白效果，增加分页居中等示例

--- dist/pages/webIDE/demos/ui/pagination/widget.js
+++ dist/pages/webIDE/demos/ui/pagination/widget.js
@@ -1,40 +1,142 @@
   var element = document.getElementById('pagination');
-    var comp = new u.pagination({el:element,jumppage:true});
-    comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200});
+  var comp = new u.pagination({
+      el: element,
+      jumppage: true
+  });
+  comp.update({
+      totalPages: 100,
+      pageSize: 20,
+      currentPage: 1,
+      totalCount: 200
+  });
 
-    this.comp.on('pageChange', function (pageIndex) {
-        console.log('新的页号为' + pageIndex);
-    });
+  this.comp.on('pageChange', function(pageIndex) {
+      console.log('新的页号为' + pageIndex);
+  });
 
-    this.comp.on('sizeChange', function (arg) {
-        console.log('每页显示条数为' + arg[0]);
-    });
-    //无边框分页
-    var paginationNoBorder = document.getElementById('paginationNoBorder');
-    var comp = new u.pagination({el:paginationNoBorder,jumppage:true});
-    comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200});
+  this.comp.on('sizeChange', function(arg) {
+      console.log('每页显示条数为' + arg[0]);
+  });
+  //无边框分页
+  var paginationNoBorder = document.getElementById('paginationNoBorder');
+  var comp = new u.pagination({
+      el: paginationNoBorder,
+      jumppage: true
+  });
+  comp.update({
+      totalPages: 100,
+      pageSize: 20,
+      currentPage: 1,
+      totalCount: 200
+  });
 
-     //有间距的分页
-    var paginationGap = document.getElementById('paginationGap');
-    var comp = new u.pagination({el:paginationGap,jumppage:true});
-    comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200});
+  //有间距的分页
+  var paginationGap = document.getElementById('paginationGap');
+  var comp = new u.pagination({
+      el: paginationGap,
+      jumppage: true
+  });
+  comp.update({
+      totalPages: 100,
+      pageSize: 20,
+      currentPage: 1,
+      totalCount: 200
+  });
 
-    //大尺寸分页
-    var paginationLg = document.getElementById('paginationLg');
-    var comp = new u.pagination({el:paginationLg,jumppage:true});
-    comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200,showState:false});
+  //大尺寸分页
+  var paginationLg = document.getElementById('paginationLg');
+  var comp = new u.pagination({
+      el: paginationLg,
+      jumppage: true
+  });
+  comp.update({
+      totalPages: 100,
+      pageSize: 20,
+      currentPage: 1,
+      totalCount: 200,
+      showState: false
+  });
 
-     //默认尺寸分页
-    var paginationDefault = document.getElementById('paginationDefault');
-    var comp = new u.pagination({el:paginationDefault,jumppage:true});
-    comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200,showState:false});
+  //默认尺寸分页
+  var paginationDefault = document.getElementById('paginationDefault');
+  var comp = new u.pagination({
+      el: paginationDefault,
+      jumppage: true
+  });
+  comp.update({
+      totalPages: 100,
+      pageSize: 20,
+      currentPage: 1,
+      totalCount: 200,
+      showState: false
+  });
 
-     //小尺寸分页
-    var paginationSm = document.getElementById('paginationSm');
-    var comp = new u.pagination({el:paginationSm,jumppage:true});
-    comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200,showState:false});
+  //小尺寸分页
+  var paginationSm = document.getElementById('paginationSm');
+  var comp = new u.pagination({
+      el: paginationSm,
+      jumppage: true
+  });
+  comp.update({
+      totalPages: 100,
+      pageSize: 20,
+      currentPage: 1,
+      totalCount: 200,
+      showState: false
+  });
 
-    //更换颜色分页
-   var paginationSm = document.getElementById('paginationColor');
-   var comp = new u.pagination({el:paginationSm,jumppage:true});
-   comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200,showState:false});
+  //更换颜色分页
+  var paginationSm = document.getElementById('paginationColor');
+  var comp = new u.pagination({
+      el: paginationSm,
+      jumppage: true
+  });
+  comp.update({
+      totalPages: 100,
+      pageSize: 20,
+      currentPage: 1,
+      totalCount: 200,
+      showState: false
+  });
+
+  //居中分页
+  var element = document.getElementById('pagination-center');
+  var comp = new u.pagination({
+      el: element,
+      jumppage: true
+  });
+  comp.update({
+      totalPages: 100,
+      pageSize: 20,
+      currentPage: 1,
+      totalCount: 200
+  });
+
+  this.comp.on('pageChange', function(pageIndex) {
+      console.log('新的页号为' + pageIndex);
+  });
+
+  this.comp.on('sizeChange', function(arg) {
+      console.log('每页显示条数为' + arg[0]);
+  });
+
+  //居右分页
+  var element = document.getElementById('pagination-right');
+  var comp = new u.pagination({
+      el: element,
+      jumppage: true
+  });
+  comp.update({
+      totalPages: 100,
+      pageSize: 20,
+      currentPage: 1,
+      totalCount: 200
+  });
+
+  this.comp.on('pageChange', function(pageIndex) {
+      console.log('新的页号为' + pageIndex);
+  });
+
+  this.comp.on('sizeChange', function(arg) {
+      console.log('每页显示条数为' + arg[0]);
+  });
